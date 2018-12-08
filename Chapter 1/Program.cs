bb1f29fac6b3485659d5edd87932d5df3f5e86b5
Mohammad M. Saffari <mmsaffari@gmail.com>
2018-12-08 18:20:06 +0330

    Exercise 1 - Done

---
+++ Chapter 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Chapter_1 {
+	class Program {
+		static void Main(string[] args) {
+			//General input variables used in different exercises.
+			//متغیر های ورودی عمومی که در تمرینهای مختلف استفاده خواهند شد.
+			int[] Array = new int[] { 11, 8, 70, 84, 65, 19, 39, 35, 88, 87, 86, 3, 74, 90, 2 };
+
+			//Exercise 1:
+			Console.WriteLine("Max: {0}", Max(Array));
+
+		}
+
+		static int Max(int[] Array) {
+			int Max = Array[0];
+			for (int i = 0; i < Array.Length; i++) {
+				if (Array[i] > Max) {
+					Max = Array[i];
+				}
+			}
+			return Max;
+		}
+	}
+}
