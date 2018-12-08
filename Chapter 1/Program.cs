cfee7f2b80646fd6b972d2eb61cc068cb5c0b3d5
Mohammad M. Saffari <mmsaffari@gmail.com>
2018-12-09 01:36:49 +0330

    Introducing functions Warming up to solve exercise 2 :)

--- Chapter 1/Program.cs
+++ Chapter 1/Program.cs
@@ -10,6 +10,20 @@ namespace Chapter_1 {
 			//Exercise 1:
 			Console.WriteLine("Max: {0}", Max(Array));
 
+			//Functions Introduction:
+			// معرفی توابع
+			int[] A = new int[] { 342, 329, 616, 131, 249, 254, 898, 398, 968, 403, 143, 288, 313, 636, 267 };
+			int[] B = new int[] { 81, 470, 831, 232, 432, 989, 58, 998, 587, 321, 725, 330, 632 };
+			int[] C = new int[] { 890, 313, 786, 354, 508, 787, 515, 608, 51, 15, 116 };
+
+			Console.WriteLine("Max(A) = {0}", Max(A));
+			Console.WriteLine("Min(A) = {0}", Min(A));
+			Console.WriteLine("Max(B) = {0}", Max(B));
+			Console.WriteLine("Min(B) = {0}", Min(B));
+			Console.WriteLine("Max(C) = {0}", Max(C));
+			Console.WriteLine("Min(C) = {0}", Min(C));
+
+
 		}
 
 		static int Max(int[] Array) {
@@ -20,6 +34,16 @@ namespace Chapter_1 {
 				}
 			}
 			return Max;
+		}
+
+		static int Min(int[] Array) {
+			int Min = Array[0];
+			for (int i = 0; i < Array.Length; i++) {
+				if (Array[i] < Min) {
+					Min = Array[i];
+				}
+			}
+			return Min;
 		}
 	}
 }
