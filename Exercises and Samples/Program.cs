10cbb49a5e3b1b58b117fc45045d3918f0fb8153
Mahdi Saffari <mmsaffari@gmail.com>
2018-12-31 12:39:59 +0330

    Exercises has been started.

---
+++ Exercises and Samples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercises_and_Samples {
+	class Program {
+		static void Main(string[] args) {
+			Console.Write("Please enter a number: ");
+			long N = Convert.ToInt64(Console.ReadLine());
+			PrintNumbers(N);
+		}
+
+
+		static void PrintNumbers(long number) {
+			while (number > 0) {
+				Console.WriteLine(number % 10);
+				number /= 10;
+			}
+		}
+	}
+}
