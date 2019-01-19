4b29d5fd1ab931927f62e55ede5532287b22c1d7
Mohammad M. Saffari <mmsaffari@gmail.com>
2019-01-19 03:57:31 +0330

    Search and Sort Methods' repo has been added.

---
+++ Exercises and Samples/SearchMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercises_and_Samples {
+	public class SearchMethods {
+		public static int BinarySearch_Recursive<T>(T[] input, T item, int lowerBound, int upperBound) where T : IComparable {
+			if (lowerBound <= upperBound) {
+				int mid = (lowerBound + upperBound) / 2;
+				if (input[mid].CompareTo(item) == 0) {
+					return mid;
+				} else if (input[mid].CompareTo(item) > 0) {
+					return BinarySearch_Recursive(input, item, lowerBound, mid - 1);
+				} else {
+					return BinarySearch_Recursive(input, item, mid + 1, upperBound);
+				}
+			} else {
+				return -1;
+			}
+		}
+
+		public static int BinarySearch<T>(T[] input, T item) where T : IComparable {
+			int low = 0;
+			int high = input.Length;
+
+			while (low <= high) {
+				int mid = (low + high) / 2;
+				if (input[mid].CompareTo(item) == 0) {
+					return mid;
+				} else if (input[mid].CompareTo(item) > 0) {
+					high = mid - 1;
+				} else {
+					low = mid + 1;
+				}
+			}
+			return -1;
+		}
+	}
+}
---
+++ Exercises and Samples/SortMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercises_and_Samples {
+	public class SortMethods {
+		public static T[] InsertionSort<T>(T[] input) where T : IComparable {
+			T[] result = input.Clone() as T[];
+			for (int i = 1; i < result.Length; i++) {
+				T key = result[i];
+				int j = i - 1;
+				while (j > 0 && result[j].CompareTo(key) > 0) {
+					result[j + 1] = result[j--];
+				}
+				result[j + 1] = key;
+			}
+			return result;
+		}
+
+
+		public static T[] MergeSort<T>(T[] input) where T : IComparable {
+			int n = input.Length;
+
+			if (n > 1) {
+				int h = n / 2;
+				T[] U = MergeSort(input.Take(h).ToArray());
+				T[] V = MergeSort(input.Skip(h).ToArray());
+
+				T[] result = new T[U.Length + V.Length];
+				int uIdx = 0;
+				int vIdx = 0;
+				int rIdx = 0;
+				while (uIdx < U.Length || vIdx < V.Length) {
+					result[rIdx++] = U[uIdx].CompareTo(V[vIdx]) < 0 ? U[uIdx++] : V[vIdx++];
+					if (uIdx == U.Length) {
+						for (int i = vIdx; i < V.Length; i++) {
+							result[rIdx++] = V[vIdx++];
+						}
+					}
+					if (vIdx == V.Length) {
+						for (int i = uIdx; i < U.Length; i++) {
+							result[rIdx++] = U[uIdx++];
+						}
+					}
+				}
+				return result;
+			}
+
+			return input;
+		}
+	}
+
+
+}
